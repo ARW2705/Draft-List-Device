9721ed26b309967986fbffeab25494aa0c8eeead
Andrew Wanex <awanex2705@gmail.com>
2022-06-01 13:50:13 -0700

    feature: add registration component

---
+++ src/components/Register/Register.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import './Register.css'
+
+
+function Register({ passcode }) {
+  return (
+    <h1 className="Register">
+      { passcode }
+    </h1>
+  )
+}
+
+export default Register
