f4c2caf45aff36a53f191479cfcd9c8af643092f
Andrew Wanex <awanex2705@gmail.com>
2022-06-01 13:50:37 -0700

    feature: add dots loader component

---
+++ src/components/Loaders/Dots/DotsLoader.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import './DotsLoader.css'
+
+
+function DotsLoader() {
+  return (
+    <div className="DotsLoader">
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  )
+}
+
+export default DotsLoader
